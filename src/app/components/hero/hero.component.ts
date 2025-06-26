5af061a63dc0dcc6739bce3813d25ab53d72a76d
uma <umakrish0810@gmail.com>
2025-06-26 17:49:33 +0530

    chamge

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -3,7 +3,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
