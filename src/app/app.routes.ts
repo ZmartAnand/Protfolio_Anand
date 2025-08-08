e101fdfa3eecc1df098655406c2b453b50f699d7
Anand <s.anandsiva7897@gmail.com>
2025-08-08 15:46:23 +0530

    updateD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,8 @@ import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
 
 export const routes: Routes = [
-  { path: '', component: HeroComponent },
+  { path: '', redirectTo: '/hero', pathMatch: 'full' },
+  { path: 'hero', component: HeroComponent },
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
   { path: 'projects', component: ProjectsComponent },
